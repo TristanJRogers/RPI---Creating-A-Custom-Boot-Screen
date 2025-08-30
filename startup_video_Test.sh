361f6a2d68d85b2626b9d4fd20e25dd4a9896da1
Tristan Rogers <Tristanjrogers@hotmail.com>
2025-08-29 22:45:52 -0400

    Update startup_video_Test.sh

--- startup_video_Test.sh
+++ startup_video_Test.sh
@@ -7,4 +7,8 @@ export DISPLAY=:0
 VIDEO="/home/tristanjrogers/Videos/StartupTest.mp4"
 
 # Plays the video
+# --fs for fullscreen
+# --quiet to hide any text that mpv may output
+# --really-quiet to ensure messsages and text are not shown during the video
+# --keep-open=no to close the video as soon as it ends ensuring a clean transition to the desktop
 mpv --fs -quiet --really-quiet --keep-open=no "$VIDEO"
