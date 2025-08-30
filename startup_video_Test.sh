cf596a69a4970992e979d22a5ba6d07173376e09
Tristan Rogers <Tristanjrogers@hotmail.com>
2025-08-29 22:40:06 -0400

    Update startup_video_Test.sh

--- startup_video_Test.sh
+++ startup_video_Test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# small wait to ensure the session is fully up
-
-
 # Makes sure we can talk to the display
 export DISPLAY=:0
 
-# This is the startup up video location
+# Startup up video location
 VIDEO="/home/tristanjrogers/Videos/StartupTest.mp4"
 
 # Plays the video
